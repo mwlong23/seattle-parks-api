a9adf87c0112e411e0436ac1e57cc8aabb2761b1
Mitchell <meechllada@gmail.com>
2017-12-15 09:49:10 -0800

    adds passing validation tests for park model

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,2 +1,12 @@
 class Park < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :address
+  validates_presence_of :bathroom
+  validates_presence_of :drinking_fountain
+  validates_presence_of :dog_park
+  validates_presence_of :playground
+  validates_presence_of :latitude
+  validates_presence_of :longitude
+
 end
